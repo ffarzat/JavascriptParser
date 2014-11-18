7975c857bc4a72222500edfebdb30a353d255878
Fabio Farzat <fabiofarzat@gmail.com>
2014-11-18 15:59:54 -0200

    cuspindo arquivo com as functions e argumentos

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace ConsoleApplication1
     {
         static int indent = 0;
         private static string jsFile = @"jquery-ui-1.8.24.min.js";
+
+        static List<string> _funcoes = new List<string>();
+
 
         static void Main(string[] args)
         {
@@ -31,7 +35,10 @@ namespace ConsoleApplication1
 
 
                 var tree = programReturn.Tree as CommonTree;
-                WriteTree(tree); 
+                
+                EscreverFuncoes(tree);
+
+                //WriteTree(tree); 
 
             }
             catch (Exception ex)
@@ -43,10 +50,75 @@ namespace ConsoleApplication1
             Console.ReadKey(true); 
         }
 
+        /// <summary>
+        /// Função Base para escrever as funções
+        /// </summary>
+        /// <param name="tree"></param>
+        private static void EscreverFuncoes(CommonTree tree)
+        {
+            var sw = new StreamWriter(Environment.CurrentDirectory + @"\Arvoreprocessada.txt");
+            sw.WriteLine(" │");
+            PrintTree(" ", true, tree, sw);
+            sw.Close();
+        }
 
+        /// <summary>
+        /// Itera a árvore e escreve no arquivo
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="isTail"></param>
+        /// <param name="tree"></param>
+        /// <param name="sw"></param>
+        private static void PrintTree(string prefix, bool isTail, CommonTree tree, StreamWriter sw) 
+        {
+
+            if (tree.Type == 17) //function
+            {
+                sw.WriteLine(prefix + "└── " + tree.Text);
+
+                if (tree.ChildCount > 0)
+                {
+                    foreach (CommonTree child in tree.Children)
+                    {
+                        if(child.Type != 113)
+                            sw.WriteLine(prefix + "    ├── " + child.Text);
+
+                        if (child.Type == 111)//ARGS
+                        {
+                            if (child.ChildCount > 0)
+                            {
+                                foreach (CommonTree child2 in child.Children)
+                                {
+                                    sw.WriteLine(prefix + "        ├── " + child2.Text);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+
+            
+
+            if (tree.ChildCount > 0)
+            {
+                foreach (CommonTree child in tree.Children)
+                {
+                    PrintTree(prefix + (isTail ? "    " : "│   "), false, child, sw);
+                }
+            }
+
+        } 
+
+        /// <summary>
+        /// Escreve a Arvore no console
+        /// </summary>
+        /// <param name="tree"></param>
         static void WriteTree(CommonTree tree) 
         { 
-            WriteLine("Tree.Text: " + tree.Text); 
+            WriteLine("Tree.Text: " + tree.Text);
             WriteLine("Tree.Type: " + tree.Type); 
             WriteLine("Tree.Line: " + tree.Line); 
             WriteLine("Tree.CharPositionInLine: " + tree.CharPositionInLine); 
