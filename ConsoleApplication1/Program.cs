4d9665af95c403494d5118252e2b0258d3b12786
ffarzat <fabiofarzat@gmail.com>
2015-07-07 20:08:49 -0300

    testando afinidade com cpu

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -128,6 +128,23 @@ namespace ConsoleApplication1
             var sw = new Stopwatch();
             var swEpoch = new Stopwatch();
             sw.Start();
+            
+            #region CPU
+            
+            int processors = 1;
+            
+            string processorsStr = System.Environment.GetEnvironmentVariable("NUMBER_OF_PROCESSORS");
+            
+            if (processorsStr != null)
+                processors = int.Parse(processorsStr);
+
+            Console.WriteLine("{0} processadores detectados", processors);
+
+
+            System.Diagnostics.Process.GetCurrentProcess().ProcessorAffinity = (System.IntPtr)1; //forço o console a ficar no CPU 1
+
+            #endregion 
+
 
             #region Encontra a função alvo da otimização, recupera o bloco de instruções
             var funcaoOtimizar = JavascriptAstCodeGenerator.FindFunctionTree(tree, TargetFunction);
