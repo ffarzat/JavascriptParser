b37aced2fd1d824177659bb8e4a27fb9426821c6
ffarzat <fabiofarzat@gmail.com>
2015-07-11 22:29:23 -0300

    Tratamento de erro no crossover

--- ConsoleApplication1/JavascriptChromosome.cs
+++ ConsoleApplication1/JavascriptChromosome.cs
@@ -366,13 +366,18 @@ namespace ConsoleApplication1
                 var blockDad = _function.GetChild(2);
                 var blockMom = javascriptChromosomePair.Function.GetChild(2);
 
+                if (blockDad.ChildCount == 0 || blockMom.ChildCount == 0)
+                    break;
+                
                 int dadLine = Rand.Next(0, blockDad.ChildCount); //at line instruction
                 int momLine = Rand.Next(0, blockMom.ChildCount); //at line instruction
 
+                if (blockDad.GetChild(dadLine).ChildCount == 0 || blockMom.GetChild(momLine).ChildCount == 0)
+                    break;
+
                 int dadPoint = Rand.Next(0, blockDad.GetChild(dadLine).ChildCount); //at instruction Level
                 int momPoint = Rand.Next(0, blockMom.GetChild(momLine).ChildCount); //at instruction Level
-
-
+                
                 var functionNodeDad = blockDad.GetChild(dadLine).GetChild(dadPoint);//function node at Dad
                 var functionNodeMom = blockMom.GetChild(momLine).GetChild(momPoint);//function node at Mom
 
