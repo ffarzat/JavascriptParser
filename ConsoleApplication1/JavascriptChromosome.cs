f5b3abde19e56715f467f2d8b39c8fa45216c9a7
ffarzat <fabiofarzat@gmail.com>
2015-07-12 13:36:17 -0300

    Instrumentando o código para saber como o novo  individuo é gerado

--- ConsoleApplication1/JavascriptChromosome.cs
+++ ConsoleApplication1/JavascriptChromosome.cs
@@ -141,9 +141,11 @@ namespace ConsoleApplication1
             {
                 case 0:
                     newChromosome.Delete();
+                    Console.WriteLine("         Criando novo individuo por {0}", "delete");
                     break;
                 case 1:
                     newChromosome.Mutate();
+                    Console.WriteLine("         Criando novo individuo por {0}", "mutação");
                     break;
             }
            
