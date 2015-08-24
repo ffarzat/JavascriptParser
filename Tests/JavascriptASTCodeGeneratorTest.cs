f745c7931862c80ddb2da01d6bf504d569514f60
ffarzat <fabiofarzat@gmail.com>
2015-08-24 17:54:36 -0300

    Usando a V8 para os testes do moment

--- Tests/JavascriptASTCodeGeneratorTest.cs
+++ Tests/JavascriptASTCodeGeneratorTest.cs
@@ -5,8 +5,7 @@ using System.Linq;
 using Antlr.Runtime;
 using Antlr.Runtime.Tree;
 using ConsoleApplication1;
-using Jurassic;
-using Jurassic.Library;
+using Microsoft.ClearScript.V8;
 using NUnit.Framework;
 using Xebic.Parsers.ES3;
 
@@ -19,6 +18,10 @@ namespace Tests
         private const string JsFileWithouComments = @"scriptData_sem_comentarios.js";   
         private static string _javascriptTextWithoutComments = "";
         private static CommonTree _tree;
+
+
+        public static int Total { get; set; }
+        public static int Sucesso { get; set; }
 
         [TestFixtureSetUp]
         public void Setup()
@@ -130,103 +133,37 @@ namespace Tests
         /// <summary>
         /// Execute the tests of MomentJs
         /// </summary>
-        [Ignore]
+        [Test]
         public void RunTestsFromMomentJs()
         {
-            const string jsTestFile = "tests.js";
-            const string qunitFile = "qunit-1.18.0.js";
-            const string fileMomentPath = "moment.js";
+            string jsTestFile = File.ReadAllText("tests.js");
+            string qunitFile = File.ReadAllText("qunit-1.18.0.js");
+            string fileMomentPath = File.ReadAllText("moment.js");
+
+            var engine = new V8ScriptEngine();
+
+            engine.AddHostType("helper", typeof(JavascriptAstCodeGeneratorTest));
+            engine.Execute(fileMomentPath);
+            engine.Execute(qunitFile);
+            engine.Execute(jsTestFile);
+
             
-            var engine = new Jurassic.ScriptEngine();
-            engine.SetGlobalFunction("alert", new DAlertDelegate(Console.WriteLine));
-
-            engine.ExecuteFile(fileMomentPath);
-            engine.ExecuteFile(qunitFile);
-            engine.ExecuteFile(jsTestFile);
-
-            #region Debug of tests
-
-//            try
-//            {
-//                engine.Execute(@"   moment.locale('af');
-//                                module('unit tests');
-//
-//    test('creating with utc and no arguments', function (assert) {
-//        var startOfTest = new Date().valueOf(),
-//            momentDefaultUtcTime = moment.utc().valueOf(),
-//            afterMomentCreationTime = new Date().valueOf();
-//		
-//		alert('startOfTest='+startOfTest);
-//		alert('momentDefaultUtcTime='+momentDefaultUtcTime);
-//			
-//        assert.ok(startOfTest <= momentDefaultUtcTime, 'moment UTC default time should be now, not in the past');
-//        assert.ok(momentDefaultUtcTime <= afterMomentCreationTime, 'moment UTC default time should be now, not in the future');
-//    });
-//
-//            ");
-
-
-//            }
-//            catch (JavaScriptException ex)
-//            {
-//                Console.WriteLine(string.Format("Script error in \'{0}\', line: {1}\n{2}", ex.SourcePath, ex.LineNumber, ex.Message));
-//            }
-            
-            #endregion
-
-            try
-            {
-                #region registra os retornos dos testes
+           #region registra os retornos dos testes
                 engine.Execute(@"   
                                     var total, sucesso, falha;
 
                                     QUnit.done(function( details ) {
-                                    alert('=============================================');
-                                    alert('Total:' + details.total);
-                                    alert('Falha:' + details.failed);
-                                    alert('Sucesso:' + details.passed);
-                                    alert('Tempo:' + details.runtime);
+                                    //alert('=============================================');
+                                    //alert('Total:' + details.total);
+                                    //alert('Falha:' + details.failed);
+                                    //alert('Sucesso:' + details.passed);
+                                    //alert('Tempo:' + details.runtime);
                                        
-                                    total = details.total;
-                                    sucesso = details.passed;
-                                    falha = details.failed;
+                                    helper.Total = details.total;
+                                    helper.Sucesso = details.passed;
+                                    //falha = details.failed;
 
                                 });
-
-/*
-
-                                QUnit.testDone(function( details ) {
-                                    if(details.failed > 0)
-                                    {
-                                        alert('=============================================');
-                                        alert('Modulo:' + details.module);
-                                        alert('Teste:' + details.name);
-                                        alert(' Falha:' + details.failed);
-                                        alert(' Total:' + details.total);
-                                        alert(' Tempo:' + details.duration);
-                                    }
-                                });
-*/
-
-                                QUnit.log(function( details ) {
-                                  if ( details.result ) {
-                                    return;
-                                  }
-                                  var loc = details.module + ': ' + details.name + ': ',
-                                    output = 'FAILED: ' + loc + ( details.message ? details.message + ', ' : '' );
- 
-                                  if ( details.actual ) {
-                                    output += 'expected: ' + details.expected + ', actual: ' + details.actual;
-                                  }
-                                  if ( details.source ) {
-                                    output += ', ' + details.source;
-                                  }
-
-                                    alert('=============================================');
-                                    alert( output );
-                                });
-
-
 
                                 QUnit.config.autostart = false;
                                 QUnit.config.ignoreGlobalErrors = true;
@@ -236,22 +173,9 @@ namespace Tests
                 engine.Execute(@"   QUnit.load();
                                     QUnit.start();
                 ");
-            }
-            catch (JavaScriptException ex)
-            {
-                Console.WriteLine(string.Format("Script error in \'{0}\', line: {1}\n{2}", ex.SourcePath, ex.LineNumber, ex.Message));
-                throw ex;
-            }
-            catch (Exception)
-            {
-                
-                throw;
-            }
+            
 
-            var total = engine.GetGlobalValue<int>("total");
-            var sucesso = engine.GetGlobalValue<int>("sucesso");
-
-            Assert.AreEqual(total, sucesso);
+            Assert.AreEqual(Total, Sucesso);
 
         }
 
@@ -262,10 +186,10 @@ namespace Tests
         [Test]
         public void RunTestsFromGeneratedMomentJs()
         {
-            const string jsTestFile = "tests.js";
-            const string qunitFile = "qunit-1.18.0.js";
-            const string fileMomentPath = "moment.js";
-            const string fileGeneratedCode = "target.js";
+            string jsTestFile = File.ReadAllText("tests.js");
+            string qunitFile = File.ReadAllText("qunit-1.18.0.js");
+            string fileMomentPath = File.ReadAllText("moment.js");
+            string fileGeneratedCode = "target.js";
             
             var sw = new Stopwatch();
             var swTotal = new Stopwatch();
@@ -274,7 +198,7 @@ namespace Tests
 
             sw.Start();
             #region Build the AST from Js and Generate the code
-            var momentTextWithoutComments = File.ReadAllText(fileMomentPath);
+            var momentTextWithoutComments = fileMomentPath;
             
             var stream = new ANTLRStringStream(momentTextWithoutComments);
             var lexer = new ES3Lexer(stream);
@@ -292,148 +216,77 @@ namespace Tests
 
             sw.Reset();
             sw.Start();
-            var engine = new Jurassic.ScriptEngine();
-            engine.SetGlobalFunction("alert", new DAlertDelegate(Console.WriteLine));
+            var engine = new V8ScriptEngine();
+            engine.AddHostType("helper", typeof(JavascriptAstCodeGeneratorTest));
             
             sw.Stop();
             Console.WriteLine("{0} - {1}", "Engine criada e configurada", sw.Elapsed.ToString("mm\\:ss\\.ff"));
 
             sw.Reset();
             sw.Start();
-            engine.ExecuteFile(fileGeneratedCode);
+            engine.Execute(generatedJsCode);
             sw.Stop();
             Console.WriteLine("{0} - {1}", "Individuo novo carregado", sw.Elapsed.ToString("mm\\:ss\\.ff"));
             
             sw.Reset();
             sw.Start();
-            engine.ExecuteFile(qunitFile);
-            Console.WriteLine("{0} - {1}", qunitFile, sw.Elapsed.ToString("mm\\:ss\\.ff"));
+            engine.Execute(qunitFile);
+            Console.WriteLine("{0} - {1}", "", sw.Elapsed.ToString("mm\\:ss\\.ff"));
             sw.Stop();
 
             sw.Reset();
             sw.Start();
-            engine.ExecuteFile(jsTestFile);
+            engine.Execute(jsTestFile);
             sw.Stop();
-            Console.WriteLine("{0} - {1}", jsTestFile, sw.Elapsed.ToString("mm\\:ss\\.ff"));
+            Console.WriteLine("{0} - {1}", "", sw.Elapsed.ToString("mm\\:ss\\.ff"));
             
 
-            try
-            {
-                #region registra os retornos dos testes
-                sw.Reset();
-                sw.Start();
-                
-                engine.Execute(@"   
+            sw.Reset();
+            sw.Start();
 
-                                    QUnit.config.autostart = false;
-                                    QUnit.config.ignoreGlobalErrors = true;
-                                    QUnit.config.moduleFilter = 'zones';
+            #region registra os retornos dos testes
+            engine.Execute(@"   
+                                var total, sucesso, falha;
 
-                                    var total, sucesso, falha;
-
-                                    QUnit.done(function( details ) {
-                                    alert('=============================================');
-                                    alert('Total:' + details.total);
-                                    alert('Falha:' + details.failed);
-                                    alert('Sucesso:' + details.passed);
-                                    alert('Tempo:' + details.runtime);
+                                QUnit.done(function( details ) {
+                                //alert('=============================================');
+                                //alert('Total:' + details.total);
+                                //alert('Falha:' + details.failed);
+                                //alert('Sucesso:' + details.passed);
+                                //alert('Tempo:' + details.runtime);
                                        
-                                    total = details.total;
-                                    sucesso = details.passed;
-                                    falha = details.failed;
+                                helper.Total = details.total;
+                                helper.Sucesso = details.passed;
+                                //falha = details.failed;
 
-                                });
+                            });
 
-/*
-                                QUnit.moduleDone(function( details ) {
-                                    if(details.failed > 0)
-                                    {
-                                        alert('=============================================');
-                                        alert('Modulo:' + details.name);
-                                        alert('Testes:' + details.tests);
-                                        alert('Falha:' + details.failed);
-                                        alert('Sucesso:' + details.passed);
-                                        alert('Total:' + details.total);
-                                        alert('Tempo:' + details.runtime);
-                                    }
-                                });
-*/
+                            QUnit.config.autostart = false;
+                            QUnit.config.ignoreGlobalErrors = true;
+                    ");
+            #endregion
 
 
-                                QUnit.testDone(function( details ) {
-                                    if(details.failed > 0)
-                                    {
-                                        alert('=============================================');
-                                        alert('Modulo:' + details.module);
-                                        alert('Teste:' + details.name);
-                                        alert(' Falha:' + details.failed);
-                                        alert(' Total:' + details.total);
-                                        alert(' Tempo:' + details.duration);
-                                    }
-                                });
+            sw.Reset();
+            sw.Start();
 
+            engine.Execute(@"   
+                                QUnit.config.autostart = false;
+                                QUnit.config.ignoreGlobalErrors = true;
+                                //QUnit.config.moduleFilter = ""zones"";
+            ");
 
-
-                                QUnit.log(function( details ) {
-                                  if ( details.result ) {
-                                    return;
-                                  }
-                                  var loc = details.module + ': ' + details.name + ': ',
-                                    output = 'FAILED: ' + loc + ( details.message ? details.message + ', ' : '' );
- 
-                                  if ( details.actual ) {
-                                    output += 'expected: ' + details.expected + ', actual: ' + details.actual;
-                                  }
-                                  if ( details.source ) {
-                                    output += ', ' + details.source;
-                                  }
-
-                                    alert('=============================================');
-                                    alert( output );
-                                });
-
-
-                        ");
-
-                sw.Stop();
-                Console.WriteLine("{0} - {1}", "Qunit config", sw.Elapsed.ToString(@"m\:ss"));
-                #endregion
-
-
-                sw.Reset();
-                sw.Start();
-
-                engine.Execute(@"   
-                                    QUnit.config.autostart = false;
-                                    QUnit.config.ignoreGlobalErrors = true;
-                                    //QUnit.config.moduleFilter = ""zones"";
-                ");
-
-                engine.Execute(@"   QUnit.load();
-                                    QUnit.start();
-                ");
+            engine.Execute(@"   QUnit.load();
+                                QUnit.start();
+            ");
                 
-                sw.Stop();
-                Console.WriteLine("{0} - {1}", "Execução dos testes", sw.Elapsed.ToString("mm\\:ss\\.ff"));
-            }
-            catch (JavaScriptException ex)
-            {
-                Console.WriteLine(string.Format("Script error in \'{0}\', line: {1}\n{2}", ex.SourcePath, ex.LineNumber, ex.Message));
-                throw ex;
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
-            var total = engine.GetGlobalValue<int>("total");
-            var sucesso = engine.GetGlobalValue<int>("sucesso");
-
+            sw.Stop();
+            Console.WriteLine("{0} - {1}", "Execução dos testes", sw.Elapsed.ToString("mm\\:ss\\.ff"));
             
-            swTotal.Stop();
+            
+            
             Console.WriteLine("{0} - {1}", "Tempo total", swTotal.Elapsed.ToString("mm\\:ss\\.ff"));
-            Assert.AreEqual(total, sucesso);
+            Assert.AreEqual(Total, Sucesso);
 
         }
 
@@ -467,6 +320,7 @@ namespace Tests
             Assert.AreNotSame(clone, anotherClone);
             Assert.AreNotSame(anotherClone, cloneofClone);
         }
+
     }
 
 }
